5ce943684e8ea41b8685bec3e3fc2b93b3efc9b4
Denizhan Gonen <gonendenizhan@gmail.com>
2021-03-26 15:57:35 +0300

    minor change

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
 
-    [SerializeField] int timeToWait = 4;
+    [SerializeField] int timeToWait = 5;
     int currentSceneIndex;
     // Start is called before the first frame update
     void Start()
