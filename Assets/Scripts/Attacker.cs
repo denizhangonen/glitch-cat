688f9ab6064829106925000ba8ddfec757655999
Denizhan Gonen <gonendenizhan@gmail.com>
2021-03-26 18:12:12 +0300

    More Attackers implemented

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)]
-    [SerializeField] float walkSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    float currentSpeed = 1f;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         // Move move move every frame
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
 
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
