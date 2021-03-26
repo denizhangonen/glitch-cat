0b9162501ca058785d8f87e45f8b88e9b8b49aca
Denizhan Gonen <gonendenizhan@gmail.com>
2021-03-27 01:07:57 +0300

    prjectile, health, collider, rigidbody 2d implementation

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] float speed = 1f;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float damage = 50;
+
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        Debug.Log("dealing damage here!");
+        health.DealDamage(damage);
     }
 }
